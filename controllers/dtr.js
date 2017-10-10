d0de0841a779f3788228a9974677cd7630fb22ed
Sly Flores <sly@christian.com.ph>
2017-10-10 21:34:17 +0800

    started dtr

---
+++ controllers/dtr.js
@@ -0,0 +1,28 @@
+var app = angular.module('dtr',['account-module','toggle-fullscreen','dtr-module','ui.bootstrap']);
+
+app.controller('dtrCtrl',function($scope,fullscreen,form) {
+	
+	$scope.views = {};	
+	
+	form.data($scope); // initialize data	
+	
+	$scope.views.title = 'DTRs';
+	$scope.views.panel_title = 'Sly Flores';
+	
+	$scope.fullscreen = fullscreen;
+	
+	form.list($scope);
+
+	$scope.form = form;	
+
+});
+
+app.filter('pagination', function() {
+	  return function(input, currentPage, pageSize) {
+	    if(angular.isArray(input)) {
+	      var start = (currentPage-1)*pageSize;
+	      var end = currentPage*pageSize;
+	      return input.slice(start, end);
+	    }
+	  };
+});
---
+++ modules/dtr.js
@@ -0,0 +1,197 @@
+angular.module('dtr-module', ['ui.bootstrap','bootstrap-modal']).factory('form', function($http,$timeout,$compile,bootstrapModal) {
+	
+	function form() {
+		
+		var self = this;
+		
+		self.data = function(scope) { // initialize data
+			
+			scope.formHolder = {};
+			
+			scope.views.list = false;			
+			
+			scope.staff = {};
+			scope.staff.id = 0;
+
+			scope.staffs = [];
+			scope.suggest_staffs = [];
+			
+			scope.btns = {
+				ok: {
+					disabled: false,					
+					label: 'Save'					
+				},
+				cancel: {
+					disabled: false,					
+					label: 'Cancel'		
+				}
+			};
+			
+		};
+		
+		function validate(scope) {
+			
+			var controls = scope.formHolder.staff.$$controls;
+			
+			angular.forEach(controls,function(elem,i) {
+				
+				if (elem.$$attr.$attr.required) elem.$touched = elem.$invalid;
+									
+			});
+
+			return scope.formHolder.staff.$invalid;
+			
+		};
+		
+		function mode(scope,row) {
+			
+			if (row != null) {
+				scope.views.panel_title = 'Edit Staff Info';			
+				scope.btns.ok.disabled = true;
+				scope.btns.ok.label = 'Update';
+				scope.btns.cancel.label = 'Close';			
+			} else {
+				scope.views.panel_title = 'Add Staff';
+				scope.btns.ok.disabled = false;	
+				scope.btns.ok.label = 'Save';
+				scope.btns.cancel.label = 'Cancel';
+			}
+			
+		};		
+		
+		self.staff = function(scope,row) { // form
+			
+			scope.views.list = true;		
+			
+			mode(scope,row);			
+			
+			$('#x_content').html('Loading...');
+			$('#x_content').load('forms/staff.html',function() {
+				$timeout(function() { $compile($('#x_content')[0])(scope); },100);				
+			});
+						
+			if (row != null) {
+				if (scope.$id > 2) scope = scope.$parent;
+				
+				$http({
+				  method: 'POST',
+				  url: 'handlers/staff-edit.php',
+				  data: {id: row.id}
+				}).then(function mySucces(response) {
+					
+					angular.copy(response.data, scope.staff);
+					if (scope.staff.birthday != null) scope.staff.birthday = new Date(scope.staff.birthday);
+					
+				}, function myError(response) {
+					 
+				  // error
+					
+				});					
+			};			
+			
+		};
+		
+		self.edit = function(scope) {
+			
+			scope.btns.ok.disabled = !scope.btns.ok.disabled;
+			
+		};		
+		
+		self.list = function(scope) {		
+			
+			scope.views.list = false;			
+			
+			scope.staff = {};
+			scope.staff.id = 0;		
+		
+			scope.currentPage = 1;
+			scope.pageSize = 15;
+			scope.maxSize = 5;				
+		
+			scope.views.panel_title = 'Sly Flores';		
+
+			$http({
+			  method: 'POST',
+			  url: 'handlers/staffs-list.php'
+			}).then(function mySucces(response) {
+				
+				angular.copy(response.data, scope.staffs);
+				scope.filterData = scope.staffs;			
+				
+			}, function myError(response) {
+				 
+			  // error
+				
+			});
+			
+			$http({
+			  method: 'POST',
+			  url: 'handlers/staffs-suggest.php'
+			}).then(function mySucces(response) {
+				
+				angular.copy(response.data, scope.suggest_staffs);
+				
+			}, function myError(response) {
+				 
+			  // error
+				
+			});			
+			
+			$('#x_content').html('Loading...');
+			$('#x_content').load('lists/dtr.html',function() {
+				$timeout(function() { $compile($('#x_content')[0])(scope); },100);				
+			});	
+
+		};
+
+		self.save = function(scope) {			
+			
+			if (validate(scope)) return;
+			
+			$http({
+			  method: 'POST',
+			  url: 'handlers/staff-save.php',
+			  data: scope.staff
+			}).then(function mySucces(response) {
+				
+				self.list(scope);
+				
+			}, function myError(response) {
+				 
+			  // error
+				
+			});		
+		
+		};
+		
+		self.delete = function(scope,row) {
+			
+			var onOk = function() {
+				
+				if (scope.$id > 2) scope = scope.$parent;			
+				
+				$http({
+				  method: 'POST',
+				  url: 'handlers/staff-delete.php',
+				  data: {id: [row.id]}
+				}).then(function mySucces(response) {
+
+					self.list(scope);
+					
+				}, function myError(response) {
+					 
+				  // error
+					
+				});
+
+			};
+
+			bootstrapModal.confirm(scope,'Confirmation','Are you sure you want to delete this record?',onOk,function() {});						
+
+		};		
+		
+	};
+	
+	return new form();
+	
+});
