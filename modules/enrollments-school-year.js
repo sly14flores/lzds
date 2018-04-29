6aff172c779f7ed1d1d1a1ca4a7fb9b314f04872
Sly Flores <sly@christian.com.ph>
2018-04-29 22:37:47 +0800

    school year enrollment/print

--- modules/enrollments-school-year.js
+++ modules/enrollments-school-year.js
@@ -1,4 +1,4 @@
-angular.module('enrollments-school-year', ['ui.bootstrap','bootstrap-modal','x-panel-module','pnotify-module','block-ui','school-year']).factory('form', function($http,$timeout,$compile,bootstrapModal,xPanel,pnotify,blockUI,schoolYear) {
+angular.module('enrollments-school-year', ['ui.bootstrap','bootstrap-modal','x-panel-module','pnotify-module','block-ui','school-year','window-open-post']).factory('form', function($http,$timeout,$compile,bootstrapModal,xPanel,pnotify,blockUI,schoolYear,printPost) {
 	
 	function form() {
 		
@@ -47,18 +47,7 @@ angular.module('enrollments-school-year', ['ui.bootstrap','bootstrap-modal','x-p
 				
 			});
 			
-			//
-			
-			scope.student_enrollment = {};
-			scope.student_enrollment.id = 0;
-			scope.enrollment_fees = [];			
-			scope.details = {
-				sub_total: 0,
-				sub_total_str: 0,
-				discount: 0,
-				total: 0,
-				total_str: 0
-			};			
+			//				
 			
 			scope.btns = {
 				edit: {
@@ -67,6 +56,20 @@ angular.module('enrollments-school-year', ['ui.bootstrap','bootstrap-modal','x-p
 			};
 			
 		};
+		
+		function validate(scope) {
+
+			var controls = scope.formHolder.enrollment.$$controls;
+			
+			angular.forEach(controls,function(elem,i) {
+				
+				if (elem.$$attr.$attr.required) elem.$touched = elem.$invalid;
+									
+			});
+
+			return scope.formHolder.enrollment.$invalid;			
+			
+		};		
 		
 		self.levelSelected = function(scope,level) {
 			
@@ -128,7 +131,42 @@ angular.module('enrollments-school-year', ['ui.bootstrap','bootstrap-modal','x-p
 		
 		self.view = function(scope,enrollee) {			
 			
+			scope.btns.edit.disabled = true;
+			
+			scope.enrollment = {};
+			scope.enrollment.id = 0;
+			scope.enrollment_fees = [];
+			scope.benrollment_fees = [];
+			scope.details = {
+				sub_total: 0,
+				sub_total_str: 0,
+				discount: 0,
+				total: 0,
+				total_str: 0
+			};			
+			
+			$http({
+			  method: 'POST',
+			  url: 'handlers/enrollment-view.php',
+			  data: {id: enrollee.id}
+			}).then(function mySucces(response) {
+
+				angular.copy(response.data.enrollment, scope.enrollment);
+				angular.copy(response.data.enrollment_fees, scope.enrollment_fees);
+				angular.copy(response.data.enrollment_fees, scope.benrollment_fees);
+				scope.sections_d = scope.enrollment.grade.sections;
+				scope.details.discount = response.data.details.discount;
+				details(scope);					
+
+			}, function myError(response) {
+				 
+			  // error
+				
+			});			
+			
 			var onOk = function() {
+				
+				self.save(scope);
 				
 			};
 			
@@ -156,8 +194,119 @@ angular.module('enrollments-school-year', ['ui.bootstrap','bootstrap-modal','x-p
 
 			});
 			
+		};
+
+		self.downloadFees = function(scope,level) {
+			
+			if (scope.btns.edit.disabled) return;
+			
+			scope.sections_d = level.sections;						
+			
+			fees(scope);
+			
 		};		
 		
+		function fees(scope) {
+
+			if (scope.enrollment.enrollment_school_year == undefined) {
+				scope.formHolder.enrollment.enrollment_school_year.$touched = true;
+				return;
+			};							
+
+			$http({
+			  method: 'POST',
+			  url: 'handlers/enrollment-fees.php',
+			  data: {school_year: scope.enrollment.enrollment_school_year, level: scope.enrollment.grade}
+			}).then(function mySucces(response) {			
+
+				scope.enrollment_fees = angular.copy(response.data);
+
+				blockUI.show("Fetching fees please wait...");				
+				angular.forEach(scope.benrollment_fees, function(item,i) {
+					scope.enrollment_fees[i]['id'] = item['id'];
+				});
+
+				blockUI.hide();
+				details(scope);
+
+			}, function myError(response) {
+				 
+			  // error
+				
+			});				
+			
+		};		
+
+		self.save = function(scope) {
+			
+			if (validate(scope)) {
+				pnotify.show('danger','Notification','Some fields are required.');
+				return;
+			}	
+			
+			$http({
+			  method: 'POST',
+			  url: 'handlers/enrollment-save.php',
+			  data: {student_enrollment: scope.enrollment, enrollment_fees: scope.enrollment_fees, details: scope.details}
+			}).then(function mySucces(response) {
+				
+				if (scope.enrollment.id == 0) {
+					pnotify.show('success','Notification','Student successfully enrolled.');
+				} else {
+					pnotify.show('success','Notification','Student enrollment info successfully updated.');				
+				}
+				
+			}, function myError(response) {
+				 
+			  // error
+				
+			});				
+			
+		};		
+		
+		function details(scope) {
+
+			scope.details.sub_total = 0;
+			angular.forEach(scope.enrollment_fees, function(item,i) {
+				scope.details.sub_total += parseFloat(item.amount);
+			});
+			
+			scope.details.total = scope.details.sub_total - scope.details.discount;
+			scope.details.sub_total_str = formatThousandsNoRounding(scope.details.sub_total,2);
+			scope.details.total_str = formatThousandsNoRounding(scope.details.total,2);
+		
+		};
+		
+		self.print = function(scope,enrollment) {
+
+			printPost.show('reports/enrollment.php',{filter:{id: enrollment.id}});
+			
+		};			
+		
+		self.total = function(scope) {
+			if (isNaN(scope.details.discount)) return;
+			scope.details.total = scope.details.sub_total - scope.details.discount;
+			scope.details.total_str = formatThousandsNoRounding(scope.details.total,2);
+		};		
+		
+		function formatThousandsWithRounding(n, dp){
+		  var w = n.toFixed(dp), k = w|0, b = n < 0 ? 1 : 0,
+			  u = Math.abs(w-k), d = (''+u.toFixed(dp)).substr(2, dp),
+			  s = ''+k, i = s.length, r = '';
+		  while ( (i-=3) > b ) { r = ',' + s.substr(i, 3) + r; }
+		  return s.substr(0, i + 3) + r + (d ? '.'+d: '');
+		};
+
+		var formatThousandsNoRounding = function(n, dp){
+		  var e = '', s = e+n, l = s.length, b = n < 0 ? 1 : 0,
+			  i = s.lastIndexOf('.'), j = i == -1 ? l : i,
+			  r = e, d = s.substr(j+1, dp);
+		  while ( (j-=3) > b ) { r = ',' + s.substr(j, 3) + r; }
+		  return s.substr(0, j + 3) + r + 
+			(dp ? '.' + d + ( d.length < dp ? 
+				('00000').substr(0, dp - d.length):e):e);
+		};		
+
 	};
 
 	return new form();
