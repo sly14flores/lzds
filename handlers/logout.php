06a3b08e9aa58013ab2ee2f3c97c66ccebaaa11a
Sly Flores <sly@christian.com.ph>
2020-05-14 15:17:14 +0800

    Updated logout

--- handlers/logout.php
+++ handlers/logout.php
@@ -5,7 +5,7 @@ session_start();
 if (isset($_SESSION['id'])) unset($_SESSION['id']);
 if (isset($_SESSION['staff'])) unset($_SESSION['staff']);
 
-echo "Logout Successful";
+// echo "Logout Successful";
 
 header("location: ../index.php");
 
