02249521506f71647aabec8e70d4b724a860c319
Sly Flores <sly@christian.com.ph>
2022-07-15 00:23:58 +0800

    ...

--- handlers/payments-list.php
+++ handlers/payments-list.php
@@ -11,6 +11,11 @@ $con = new pdo_db();
 
 $school_year = (isset($_POST['school_year']))?$_POST['school_year']:current_sy($con);
 
+$previous_sy = 0;
+if (isset($school_year['id'])) {
+	$previous_sy = $school_year['id'] - 1;
+}
+
 $and_level = "";
 
 if ((isset($_POST['level']))&&($_POST['level']['id']>0)) {
@@ -48,7 +53,33 @@ foreach ($payments as $i => $payment) {
 
 	if($payment['origin']=='walk-in') $payments[$i]['origin']='Walk-in';
 	if($payment['origin']=='online') $payments[$i]['origin']='Online';
-	
+
+	$sql = "SELECT enrollments.id,";
+	$sql .= " (SELECT students.lrn FROM students WHERE students.id = enrollments.student_id) lrn,";
+	$sql .= " enrollments.school_id,";
+	$sql .= " enrollments.origin,";
+	$sql .= " enrollments.enrollee_rn,";
+	$sql .= " students.student_status,";
+	$sql .= " (SELECT CONCAT(students.lastname, ', ', students.firstname, ' ', students.middlename) FROM students WHERE students.id = enrollments.student_id) fullname,";
+	$sql .= " (SELECT school_years.school_year FROM school_years WHERE school_years.id = enrollments.enrollment_school_year) enrollment_school_year,";
+	$sql .= " (SELECT grade_levels.description FROM grade_levels WHERE grade_levels.id = enrollments.grade) grade,";
+	$sql .= " (SELECT sections.description FROM sections WHERE sections.id = enrollments.section) section,";
+	$sql .= " DATE_FORMAT(enrollments.enrollment_date,'%M %e, %Y') enrollment_date,";
+	$sql .= " (SELECT SUM(students_fees.amount) FROM students_fees WHERE students_fees.enrollment_id = enrollments.id) sub_total,";
+	$sql .= " (SELECT students_discounts.amount FROM students_discounts WHERE students_discounts.enrollment_id = enrollments.id) discount,";
+	$sql .= " (SELECT SUM(payments.amount) FROM payments WHERE payments.enrollment_id = enrollments.id) payments,";
+	$sql .= " IFNULL((SELECT SUM(students_vouchers.amount) FROM students_vouchers WHERE students_vouchers.enrollment_id = enrollments.id),0) voucher";
+	$sql .= " FROM enrollments";
+	$sql .= " LEFT JOIN students ON enrollments.student_id = students.id";
+	$sql .= " WHERE enrollments.id = ".$payment['id']." AND enrollment_school_year = ".$previous_sy;
+
+	$_payment = $con->getData($sql);
+
+	$payments[$i]['previous_balance'] = 0;
+	if (count($_payment)) {
+		$payments[$i]['previous_balance'] = $_payment[0]['balance'] = $_payment[0]['total'] - $_payment[0]['payments'];
+	}
+
 }
 
 echo json_encode($payments);
